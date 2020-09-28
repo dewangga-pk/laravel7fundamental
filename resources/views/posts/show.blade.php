6a3d104e417bfb4f5e8f4fb9b458210c000f7f12
dewangga-pk <pacetcommunity570@gmail.com>
2020-09-28 21:10:45 +0700

    route wildcard slug

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title',$slug)
+@section('content')
+<p>
+    {{ $slug}}
+</p>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController');
 
+Route::get('posts/{slug}', 'PostController@show');
+
 Route::view('about', 'about');
 Route::view('contact', 'contact');
 Route::view('login', 'login');
