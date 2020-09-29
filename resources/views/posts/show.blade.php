ef8d0c688d402d536bb1d3f74dc79ca868d547b7
dewangga-pk <pacetcommunity570@gmail.com>
2020-09-29 10:26:17 +0700

    route model binding

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 @section('title',$post->title)
 @section('content')
-<div class="container">
-    <h1>{{ $post->title }}</h1>
-    <p>{{ $post->body }}</p>
-</div>
+    <div class="container">
+        <h1>{{ $post->title }}</h1>
+        <p>{{ $post->body }}</p>
+    </div>
 @endsection
